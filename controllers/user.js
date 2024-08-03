7fa8c3c5578c0310d1a08d5a3494673462e86f9f
karthikn1997 <karthikn951@gmail.com>
2024-08-03 15:00:03 +0530

    okkkk

--- controllers/user.js
+++ controllers/user.js
@@ -17,7 +17,17 @@ const userRegister = async (req, res) => {
       address,
     } = req.body;
 
+    const aldreadyAadharExist = await User.findOne({adhaarNumber});
+
+    if(aldreadyAadharExist){
+      return res
+        .status(400)
+        .json({ message: "Aadhaar number is already registered" });
+    }
+
     const allUsers = await User.find({});
+
+
 
     const referralId = await uniqId();
 
@@ -48,8 +58,7 @@ const userRegister = async (req, res) => {
       referredBy,
     });
 
-    console.log(newUser);
-
+    // console.log(newUser);
 
     for (const singleUser of allUsers) {
       if (singleUser.referralId === referredBy) {
@@ -81,83 +90,3 @@ const getAllUsers = async (req, res) => {
 };
 
 export { userRegister, getAllUsers };
-
-// import User from "../models/user.js";
-// import { uniqId } from "../uniqId.js";
-
-// const userRegister = async (req, res) => {
-//   try {
-//     const {
-//       name,
-//       fatherName,
-//       dob,
-//       gender,
-//       email,
-//       mobileNumber,
-//       adhaarNumber,
-//       district,
-//       constituency,
-//       address,
-//     } = req.body;
-
-//     const allUsers = await User.find({});
-
-//     const referralId = await uniqId();
-
-//     let referredBy = req.body.referredBy;
-
-//     const referralUser = await User.findOne({
-//       referralId: req.body.referredBy,
-//     });
-
-//     if (!referralUser) {
-//       referredBy = "";
-//     }
-
-//     // Create a new user
-//     const newUser = new User({
-//       name,
-//       fatherName,
-//       dob,
-//       gender,
-//       email,
-//       mobileNumber,
-//       adhaarNumber,
-//       district,
-//       constituency,
-//       address,
-//       referralId,
-//       referredBy,
-//     });
-
-//     for (const singleUser of allUsers) {
-//       if (singleUser.referralId === referredBy) {
-//         singleUser.referredPeoples.push(referralId);
-
-//         await singleUser.save();
-//       }
-//     }
-
-//     const savedUser = await newUser.save();
-//     return res.status(200).json(savedUser);
-//   } catch (err) {
-//     console.error("Error saving user:", err);
-//     if (err.code === 11000 && err.keyPattern && err.keyPattern.adhaarNumber) {
-//       res.status(400).json({ message: "Aadhaar number is already registered" });
-//     } else {
-//       res.status(500).json({ message: err.message });
-//     }
-//   }
-// };
-
-// const getAllUsers = async (req, res) => {
-//   try {
-//     const users = await User.find({});
-//     res.status(200).json(users);
-//   } catch (error) {
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
-
-// export { userRegister, getAllUsers };
-
